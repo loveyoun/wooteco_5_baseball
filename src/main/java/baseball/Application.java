605ade2448434f541fe7951e30da42451b323d71
loveyoun <lovekite0406@gmail.com>
2023-10-25 18:51:13 +0900

    test commit

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import baseball.controller.BaseballController;
 
 public class Application {
 
-
     public static void main(String[] args) {
         BaseballController baseballController =  new BaseballController();
         baseballController.gameStart();
